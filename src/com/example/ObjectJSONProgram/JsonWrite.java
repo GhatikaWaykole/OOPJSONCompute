cbe9e4f6fde924c27bd391565e0b0607b9415901
ghatikawaykole <ghatika99@gmail.com>
2021-09-21 08:25:15 +0530

    welcome to JSON compute

---
+++ src/com/example/ObjectJSONProgram/JsonWrite.java
@@ -0,0 +1,14 @@
+package com.example.ObjectJSONProgram;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonWrite {
+    public static void main(String[] args) throws IOException {
+       System.out.println("welcome to the JSON compute");
+    }
+}
