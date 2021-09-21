aae03868d0dea9eddf09d83ee3d00ad026be6ec4
ghatikawaykole <ghatika99@gmail.com>
2021-09-21 11:15:32 +0530

    Adding the inventory data

--- src/com/example/ObjectJSONProgram/JsonWrite.java
+++ src/com/example/ObjectJSONProgram/JsonWrite.java
@@ -1,14 +1,15 @@
 package com.example.ObjectJSONProgram;
 
-import jdk.nashorn.internal.parser.JSONParser;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 
 public class JsonWrite {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException, org.json.simple.parser.ParseException {
        System.out.println("welcome to the JSON compute");
         JSONParser jsonParser = new JSONParser();
         FileReader reader = new FileReader("C:\\Users\\PC\\IdeaProjects\\OOPJSONProgram\\src\\data.json");
@@ -19,10 +20,10 @@ public class JsonWrite {
         JSONArray array = (JSONArray) inventoryObj.get("inventory detail");
 
         for (int i = 0; i < array.size(); i++) {
-            JSONObject inventory = (JSONObject) array.get(i);
+            JSONObject inventory = (JSONObject)array.get(i);
             String name = (String) inventory.get("name");
-            double weight = (double) inventory.get("weight");
-            double priceKg = (double) inventory.get("priceKg");
+            double weight = Double.parseDouble(inventory.get("weight").toString());
+            double priceKg = Double.parseDouble(inventory.get("priceKg").toString());
             //   String type = (String) inventory.get("type");
 
             System.out.println("inventory detail");
